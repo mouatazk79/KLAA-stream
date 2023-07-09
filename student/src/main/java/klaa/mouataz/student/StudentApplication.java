64ba4944abe77412d6a4b8734f5f9aa8bc897b68
klaa <mouatazk79@gmail.com>
2023-07-09 11:10:33 +0100

    adding the student microservice with banner and the database and the eureka client configurations

---
+++ student/src/main/java/klaa/mouataz/student/StudentApplication.java
@@ -0,0 +1,10 @@
+package klaa.mouataz.student;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+@SpringBootApplication
+public class StudentApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(StudentApplication.class,args);
+    }
+}
---
+++ student/src/main/java/klaa/mouataz/student/model/Student.java
@@ -0,0 +1,23 @@
+package klaa.mouataz.student.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Student {
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String firstName;
+    private String lastName;
+}
