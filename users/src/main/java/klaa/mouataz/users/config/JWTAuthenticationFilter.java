d1d024f6fa2093f46a6b9e51bee865574b3273cf
klaa <mouatazk79@gmail.com>
2023-07-14 00:40:44 +0100

    now if the token is null or does not start with Bearer so we need to pass it to the next filter

--- users/src/main/java/klaa/mouataz/users/config/JWTAuthenticationFilter.java
+++ users/src/main/java/klaa/mouataz/users/config/JWTAuthenticationFilter.java
@@ -16,5 +16,11 @@ public class JWTAuthenticationFilter extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(@NonNull HttpServletRequest request,@NonNull HttpServletResponse response,@NonNull FilterChain filterChain) throws ServletException, IOException {
         final String authHeader=request.getHeader("Authorization");
+        final String jwt;
+        boolean check=(authHeader == null || !authHeader.startsWith("Bearer "));
+        if (!check){
+            filterChain.doFilter(request,response);
+            return;
+        }
     }
 }
