8647e209b95d0eaf2fcd0cf75b01aaeb7527c894
klaa <mouatazk79@gmail.com>
2023-07-14 00:35:48 +0100

    when we send a request we have to send it with a jwt token which we can find it in the header

--- users/src/main/java/klaa/mouataz/users/config/JWTAuthenticationFilter.java
+++ users/src/main/java/klaa/mouataz/users/config/JWTAuthenticationFilter.java
@@ -4,13 +4,17 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
-
+@Component
+@RequiredArgsConstructor
 public class JWTAuthenticationFilter extends OncePerRequestFilter {
     @Override
-    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-
+    protected void doFilterInternal(@NonNull HttpServletRequest request,@NonNull HttpServletResponse response,@NonNull FilterChain filterChain) throws ServletException, IOException {
+        final String authHeader=request.getHeader("Authorization");
     }
 }
