a249058294116460518aac2c544fd391b6c896d5
klaa <mouatazk79@gmail.com>
2023-07-14 00:48:58 +0100

    adding the userName variable and the service that gonna extract the userName form the jwt token using the JwtService

--- users/src/main/java/klaa/mouataz/users/config/JWTAuthenticationFilter.java
+++ users/src/main/java/klaa/mouataz/users/config/JWTAuthenticationFilter.java
@@ -13,14 +13,17 @@ import java.io.IOException;
 @Component
 @RequiredArgsConstructor
 public class JWTAuthenticationFilter extends OncePerRequestFilter {
+    private final JwtService jwtService;
     @Override
     protected void doFilterInternal(@NonNull HttpServletRequest request,@NonNull HttpServletResponse response,@NonNull FilterChain filterChain) throws ServletException, IOException {
         final String authHeader=request.getHeader("Authorization");
         final String jwt;
+        final String userName;
         if (authHeader == null || !authHeader.startsWith("Bearer ")){
             filterChain.doFilter(request,response);
             return;
         }
         jwt=authHeader.substring(7);
+        userName=jwtService.extractUserName(jwt);
     }
 }
---
+++ users/src/main/java/klaa/mouataz/users/config/JwtService.java
@@ -0,0 +1,9 @@
+package klaa.mouataz.users.config;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class JwtService {
+    public String extractUserName(String jwtToken) {
+    }
+}
