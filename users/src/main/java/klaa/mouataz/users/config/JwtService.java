a3673ab174d6eae547a7f8ebdd61bc5f6b28814a
klaa <mouatazk79@gmail.com>
2023-07-14 01:12:37 +0100

    adding the method that will extract the claims from the jwt token

--- users/src/main/java/klaa/mouataz/users/config/JwtService.java
+++ users/src/main/java/klaa/mouataz/users/config/JwtService.java
@@ -1,10 +1,30 @@
 package klaa.mouataz.users.config;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
 import org.springframework.stereotype.Service;
+
+import java.security.Key;
 
 @Service
 public class JwtService {
+    private static final String SECRET_KEY ="aWq4MYiKKoprFtYisPOXp7upttzfUOMT/3w1aOjdcSI2v/9+RrlTEXznB9yGM8U1YVV/8oh0UkSIj85XFnvVtQ==\n";
     public String extractUserName(String jwtToken) {
         return null;
+    }
+    private Claims extractAllClaims(String token){
+        return Jwts
+                .parserBuilder()
+                .setSigningKey(getSignInKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
+
+    private Key getSignInKey() {
+        byte[] keyBytes= Decoders.BASE64.decode(SECRET_KEY);
+        return Keys.hmacShaKeyFor(keyBytes);
     }
 }
