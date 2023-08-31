797a514de77190df9a56f9d875f5d5659260aa9d
klaa <mouatazk79@gmail.com>
2023-08-31 23:57:09 +0100

    completing the notification feign client

---
+++ clients/src/main/java/klaa/mouataz/clients/fraud/NotificationClient.java
@@ -0,0 +1,11 @@
+package klaa.mouataz.clients.fraud;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+@FeignClient("notification")
+public interface NotificationClient {
+    @PostMapping
+    public Notification addNotification(@RequestBody Notification notification);
+}
