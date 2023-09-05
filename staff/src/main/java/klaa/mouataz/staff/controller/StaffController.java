89e283f21bff05980e67dcbc4436f85ddac7514e
klaa <mouatazk79@gmail.com>
2023-09-05 22:21:18 +0100

    completing the staff module controller

--- staff/src/main/java/klaa/mouataz/staff/controller/StaffController.java
+++ staff/src/main/java/klaa/mouataz/staff/controller/StaffController.java
@@ -1,13 +1,32 @@
 package klaa.mouataz.staff.controller;
 
+import klaa.mouataz.staff.model.Staff;
 import klaa.mouataz.staff.service.StaffService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/notifications")
 public class StaffController {
     private final StaffService staffService;
-}
+    @GetMapping
+    public List<Staff> getAllStaffs(){
+        return staffService.getAllStaffs();
+    }
+    @GetMapping("/{staffId}")
+    public Staff getStaffById(@PathVariable("staffId") Long id){
+        return staffService.getStaffById(id);
+    }
+    @DeleteMapping("/{staffId}")
+    public void deleteStaffById(@PathVariable("staffId") Long id){
+        staffService.deleteStaffById(id);
+    }
+    @PatchMapping("/{staffId}")
+    public Staff updateStaffById(@PathVariable("staffId") Long id,@RequestBody Staff newStaff) {
+        return staffService.updateStaffById(id,newStaff);
+    }
+
+    }
