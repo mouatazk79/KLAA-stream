d6bdace71a4eb7257c84bc6f51b534acc89dd8b7
klaa <mouatazk79@gmail.com>
2024-07-13 17:33:42 +0100

    fixing the code

--- api-gateway/src/main/java/klaa/mouataz/gateway/security/SecurityConfig.java
+++ api-gateway/src/main/java/klaa/mouataz/gateway/security/SecurityConfig.java
@@ -5,22 +5,22 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
-import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.server.SecurityWebFilterChain;
 
 @Configuration
 @EnableWebFluxSecurity
 public class SecurityConfig {
     @Bean
-    public SecurityFilterChain securityFilterChain(ServerHttpSecurity serverHttpSecurity){
+    public SecurityWebFilterChain securityFilterChain(ServerHttpSecurity serverHttpSecurity){
         serverHttpSecurity
                 .csrf(ServerHttpSecurity.CsrfSpec::disable)
                 .authorizeExchange(exchange ->exchange
-                        .pathMatchers("/eureka/**")
+                        .pathMatchers("/**")
                         .permitAll()
                         .anyExchange()
                         .authenticated())
                 .oauth2ResourceServer(oAuth2->oAuth2.jwt(Customizer.withDefaults()));
-        return (SecurityFilterChain) serverHttpSecurity.build();
+        return serverHttpSecurity.build();
 
     }
 }
