f489deaacc43e279f7368c701bc07a9410ddf1ee
klaa <mouatazk79@gmail.com>
2024-09-04 20:05:41 +0100

    adding the cors configuration to the apigateway

--- api-gateway/src/main/java/klaa/mouataz/gateway/ApiGWApplication.java
+++ api-gateway/src/main/java/klaa/mouataz/gateway/ApiGWApplication.java
@@ -2,10 +2,31 @@ package klaa.mouataz.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+import java.util.Arrays;
+import java.util.List;
 
 @SpringBootApplication
 public class ApiGWApplication {
     public static void main(String[] args) {
         SpringApplication.run(ApiGWApplication.class,args);
+    }
+    @Bean
+    public CorsFilter corsFilter() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.setAllowedOrigins(Arrays.asList("http://localhost:4200")); // Use specific origin only
+        config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        config.setAllowedHeaders(Arrays.asList("*"));
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+
+        return new CorsFilter(source);
     }
 }
